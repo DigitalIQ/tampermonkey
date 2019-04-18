d5b58cb45f1f3fdcb1aca589e217cc8f15698f2a
DigitalIQ <github@digitaliq.de>
2019-04-18 15:38:25 +0200

    Update WhatsAppDarkStyle.user.js

--- WhatsAppDarkStyle.user.js
+++ WhatsAppDarkStyle.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WhatsApp Dark Style
 // @namespace    
-// @version      0.2.9
+// @version      0.2.10
 // @description  Darker Style for WhatsApp website
 // @author       Manuel Bissinger
 // @match        https://web.whatsapp.com/
